b4b5627e681af4664214305afcd0b2faf1fb6248
Wiz-TechSkillsIT <wiztechskillsit@gmail.com>
2024-10-23 11:34:41 +0530

    app structure + DB connection

---
+++ SMSApp/src/com/sms/utility/DbConnection.java
@@ -0,0 +1,49 @@
+package com.sms.utility;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+ 
+public class DbConnection {
+//db connection code 
+	
+	static Connection con; 
+	
+	
+	public static void dbConnect() {
+		/*
+		 * Load the driver 
+		*/
+			try {
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				System.out.println("driver loaded");
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+		/*
+		 * Establish the connection 
+		*/
+			try {
+				con = DriverManager.getConnection("jdbc:mysql://localhost:3306/fsd_oct_sms_db", "root", "techskillsit");
+				System.out.println("Connection est...");
+			} catch (SQLException e) {
+				 e.printStackTrace();
+			}
+	}
+	
+	public static void dbClose() {
+		/* Close the connection */
+		try {
+			con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		DbConnection.dbConnect();
+		DbConnection.dbClose();
+		
+	}
+}
+//ctrl + shft + o

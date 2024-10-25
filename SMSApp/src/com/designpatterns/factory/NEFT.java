08805ce5675f0a22ea64774152d21e20213edfce
Wiz-TechSkillsIT <wiztechskillsit@gmail.com>
2024-10-25 15:51:05 +0530

    factory pattern

---
+++ SMSApp/src/com/designpatterns/factory/NEFT.java
@@ -0,0 +1,10 @@
+package com.designpatterns.factory;
+
+public class NEFT extends Payment{
+
+	@Override
+	public void processPayment(String acctNo, double amount, TransactionType type) {
+		System.out.println("NEFT does this ");
+		
+	}
+}

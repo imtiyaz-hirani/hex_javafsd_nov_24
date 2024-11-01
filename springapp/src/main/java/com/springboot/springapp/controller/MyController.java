d25b3573836c363bcbdc62798193a2d1b55faf04
Wiz-TechSkillsIT <wiztechskillsit@gmail.com>
2024-11-01 12:42:11 +0530

    form handling

--- springapp/src/main/java/com/springboot/springapp/controller/MyController.java
+++ springapp/src/main/java/com/springboot/springapp/controller/MyController.java
@@ -3,8 +3,11 @@ package com.springboot.springapp.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
- 
+import org.springframework.web.bind.annotation.PostMapping;
+
 import com.springboot.springapp.service.MyService;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class MyController {
@@ -16,5 +19,13 @@ public class MyController {
 	public String showLogin() {
 		// System.out.println("login method in controller called....");
 		return "login";
+	}
+	
+	@GetMapping("/login-form")
+	public String readLogin(HttpServletRequest req) { //DI type 2
+		String username = req.getParameter("username");
+		String password = req.getParameter("password");
+		System.out.println(username+ "=============" + password);
+		return "dashboard";
 	}
 }

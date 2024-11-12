493e533d8cb1c05f609b63121c1721b0c37b3e07
Wiz-TechSkillsIT <wiztechskillsit@gmail.com>
2024-11-12 12:03:16 +0530

    jwt util+filter add

--- JWTSecurity/src/main/java/com/springboot/JWTSecurity/SecurityConfig.java
+++ JWTSecurity/src/main/java/com/springboot/JWTSecurity/SecurityConfig.java
@@ -1,7 +1,25 @@
 package com.springboot.JWTSecurity;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.springboot.JWTSecurity.service.UserSecurityService;
+
+import jakarta.security.auth.message.config.AuthConfig;
+ 
 
 /*
  * This class will bring JWT filter to Spring and integrate everything.
@@ -10,6 +28,48 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SecurityConfig {
 
+	@Autowired
+	private UserSecurityService userSecurityService;
+	
+	@Autowired
+	private JwtFilter jwtFilter;
+	
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+		 .csrf((csrf) -> csrf.disable())
+		 .authorizeHttpRequests(authorize -> authorize
+				 	.requestMatchers(HttpMethod.GET, "/auth/login").authenticated()
+				 	.requestMatchers(HttpMethod.POST, "/auth/switch-status/{id}").hasRole("EXECUTIVE")
+				 	.requestMatchers(HttpMethod.GET, "/api/hello").hasRole("CUSTOMER")                              
+					.requestMatchers(HttpMethod.POST, "/auth/sign-up").permitAll()  
+				.anyRequest().permitAll()
+			) 
+			.sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			
+		   .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+		 
+		return http.build();
+	}
+	
+	
+	@Bean
+	PasswordEncoder getEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	AuthenticationManager authManager(AuthenticationConfiguration config) throws Exception {
+		return config.getAuthenticationManager();
+	}
+	
+	@Bean
+	DaoAuthenticationProvider authenticationProvider(){
+		DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+		authenticationProvider.setUserDetailsService(userSecurityService);
+		authenticationProvider.setPasswordEncoder(getEncoder());
+		return authenticationProvider;
+	}
 }
 
 /*

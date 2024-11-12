bc33edbe29eaef4b108792e3dac1829d240a8935
Wiz-TechSkillsIT <wiztechskillsit@gmail.com>
2024-11-12 11:31:31 +0530

    jwt util+filter add

--- JWTSecurity/src/main/java/com/springboot/JWTSecurity/repository/UserRepository.java
+++ JWTSecurity/src/main/java/com/springboot/JWTSecurity/repository/UserRepository.java
@@ -1,5 +1,13 @@
 package com.springboot.JWTSecurity.repository;
 
-public interface UserRepository {
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.springboot.JWTSecurity.model.User;
+
+public interface UserRepository extends JpaRepository<User, Integer>{
+
+	Optional<User> findByUsername(String username);
 
 }
--- JWTSecurity/src/main/java/com/springboot/JWTSecurity/service/UserSecurityService.java
+++ JWTSecurity/src/main/java/com/springboot/JWTSecurity/service/UserSecurityService.java
@@ -1,5 +1,28 @@
 package com.springboot.JWTSecurity.service;
 
-public class UserSecurityService {
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.springboot.JWTSecurity.model.User;
+import com.springboot.JWTSecurity.repository.UserRepository;
+
+@Service
+public class UserSecurityService implements UserDetailsService{
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Optional<User> optional= userRepository.findByUsername(username);
+		if(optional.isEmpty())
+			throw new UsernameNotFoundException("Invalid Username");
+		User user = optional.get();
+		return user;
+	}
 }

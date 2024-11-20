b532bca294a29781d708ef49cc9e7c17d07c3a4d
Wiz-TechSkillsIT <wiztechskillsit@gmail.com>
2024-11-20 15:20:07 +0530

    bootstrap header

--- my-angular-app/src/app/app.component.ts
+++ my-angular-app/src/app/app.component.ts
@@ -1,57 +1,13 @@
  
-import { NgFor, NgIf } from '@angular/common';
-import { Component, OnDestroy, OnInit } from '@angular/core';
+ import { Component  } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,NgIf, NgFor],
+  imports: [ ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit, OnDestroy{
-  /** Declaration  */
-  x:any; 
-  y:any; 
-  msg:any;
-  employee:any;
-  data: any; 
-  isLoggedIn:boolean=false; 
-  numArry:any=[];
-
-  constructor(){
-    console.log('constructor gets called...');
-  }
-
-  /** Initialization  */
-  ngOnInit(): void {
-    console.log('ng-oninit gets called...')
-    this.x = 10;
-    this.y = 20;
-    this.msg="";
-    this.sayHello();
-    this.employee = {
-      name: 'harry potter',
-      city: 'london',
-      address: ""
-    };
-    this.numArry = [1,2,5,2,7,8,6];
-  }
-
-  /**Functions  */
-  sayHello(){
-    this.msg = "Hello User!!!"; 
-  }
-  /** if show address button is clcked, call this function */
-  showAddress(){
-    this.employee.address ="101 Kingston lane, thames TW31SE";
-  }
- 
-  loadData(){
-    this.data="this is sample data";
-  }
-  ngOnDestroy(): void {
-    console.log('on-destroy gets called...')
-
-  }
+export class AppComponent   {
+   
 }

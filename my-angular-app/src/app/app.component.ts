8f42ebe8c491958febea1450024b4872a4cff494
Wiz-TechSkillsIT <wiztechskillsit@gmail.com>
2024-11-20 12:10:04 +0530

    initial commit

---
+++ my-angular-app/src/app/app.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit, OnDestroy{
+  /** Declaration  */
+  x:any; 
+  y:any; 
+  msg:any;
+  employee:any;
+
+  constructor(){
+    console.log('constructor gets called...');
+  }
+
+  /** Initialization  */
+  ngOnInit(): void {
+    console.log('ng-oninit gets called...')
+    this.x = 10;
+    this.y = 20;
+    this.msg="";
+    this.sayHello();
+    this.employee = {
+      name: 'harry potter',
+      city: 'london',
+      address: ""
+    };
+  }
+
+  /**Functions  */
+  sayHello(){
+    this.msg = "Hello User!!!"; 
+  }
+  /** if show address button is clcked, call this function */
+  showAddress(){
+    this.employee.address ="101 Kingston lane, thames TW31SE";
+  }
+ 
+  ngOnDestroy(): void {
+    console.log('on-destroy gets called...')
+
+  }
+}
